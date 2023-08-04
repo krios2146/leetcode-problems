c48320316d515338261a9ce78f2e5ed27cfd137d
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-04 16:03:34 +0700

    feat: add 111 Minimum Depth of Binary Tree solution
    https://leetcode.com/problems/minimum-depth-of-binary-tree/description/

---
+++ src/main/java/me/krios2146/easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,17 @@
+package me.krios2146.easy;
+
+import me.krios2146.utils.TreeNode;
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (left == 0 || right == 0) {
+            return 1 + left + right;
+        }
+        return 1 + Math.min(left, right);
+    }
+}
