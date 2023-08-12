c41f487ae360a3a28a134f7b2adb955c6b71e975
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-12 12:36:50 +0700

    feat: add 63 Unique Paths II solution
    https://leetcode.com/problems/unique-paths-ii/description/

--- src/main/java/me/krios2146/medium/UniquePathsII.java
+++ src/main/java/me/krios2146/medium/UniquePathsII.java
@@ -1,48 +1,24 @@
 package me.krios2146.medium;
 
 public class UniquePathsII {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid[0].length == 1) {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        if (grid[0][0] == 1) {
             return 0;
         }
-        return findUniquePaths(0, 0, obstacleGrid);
-    }
+        int m = grid.length;
+        int n = grid[0].length;
 
-    private static int findUniquePaths(int x, int y, int[][] grid) {
-        if (x == grid.length - 1 && y == grid[0].length - 1) {
-            return 1;
-        }
-        if (isDownFree(x, y, grid) && isRightFree(x, y, grid)) {
-            return findUniquePaths(x + 1, y, grid) + findUniquePaths(x, y + 1, grid);
-        } else if (isDownFree(x, y, grid)) {
-            return findUniquePaths(x + 1, y, grid);
-        } else if (isRightFree(x, y, grid)) {
-            return findUniquePaths(x, y + 1, grid);
-        }
-        return 0;
-    }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
 
-    private static boolean isDownFree(int x, int y, int[][] grid) {
-        // out of grid
-        if (x + 1 > grid.length - 1) {
-            return false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 1 || (i != 0 && j != 0)) {
+                    dp[i][j] = (i > 0 ? dp[i - 1][j] : 0) + (j > 0 ? dp[i][j - 1] : 0);
+                }
+            }
         }
-        // obstacle
-        if (grid[x + 1][y] == 1) {
-            return false;
-        }
-        return true;
-    }
 
-    private static boolean isRightFree(int x, int y, int[][] grid) {
-        // out of grid
-        if (y + 1 > grid[0].length - 1) {
-            return false;
-        }
-        // obstacle
-        if (grid[x][y + 1] == 1) {
-            return false;
-        }
-        return true;
+        return dp[m - 1][n - 1];
     }
 }
